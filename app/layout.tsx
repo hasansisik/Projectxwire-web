f0c413491d63797e9f59049b833d4a45154b41da
hasansisik <gegify@gmail.com>
2024-09-05 03:36:41 +0300

    chore: Remove unused SpeedInsights component from RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Poppins } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 import { Provider } from "react-redux";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import store from "../redux/store";
 
 const poppins = Poppins({
@@ -22,7 +21,6 @@ export default function RootLayout({
       <body className={cn(poppins.className, "light")}>
         <Provider store={store}>{children}</Provider>
       </body>
-      <SpeedInsights />
     </html>
   );
 }
