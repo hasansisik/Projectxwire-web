5f35754cf917b20b5d7fc6f8ffa7cb38f2a7ee19
Hasan Şişik <sisik.hsn@gmail.com>
2025-01-15 17:36:19 +0300

    z

--- components/blog/Cta.tsx
+++ components/blog/Cta.tsx
@@ -10,7 +10,7 @@ export const Cta = () => {
             Uygulama Hakkında
           </h2>
           <p className="mt-2 font-medium text-white text-opacity-90 lg:text-l">
-            Bilgi almak ve uygulamayı indirmek için hemen tıklayın.
+            Bilgi almak ve uygulamayı indirmek için hemen tıklayın
           </p>
         </div>
         <div className="flex-shrink-0 w-full text-center lg:w-auto">
