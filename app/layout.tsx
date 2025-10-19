f154f3bf77a03e8b4efcf1f19e3492bf847c9d16
Hasan Şişik <sisik.hsn@gmail.com>
2025-10-19 16:00:20 +0300

    fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ import Script from "next/script";
 
 const poppins = Poppins({
   subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"], 
   display: "swap",
 });
 
