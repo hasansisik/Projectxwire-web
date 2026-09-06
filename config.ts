3718dfa3afc5a9f9ec14f18fddb7bf211ad59339
Hasan Şişik <sisik.hsn@gmail.com>
2026-09-06 21:59:39 +0300

    c

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
 export const server: string =
   process.env.NEXT_PUBLIC_SERVER_URL ||
-  "https://projectxwire-server-d3ccf9a62f43.herokuapp.com/v1";
+  "https://projectxwire-backend.vercel.app/v1";
